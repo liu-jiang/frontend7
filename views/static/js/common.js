d647cf58c975e77e759861f4d2cde0adc418b25c
jiang <jgx@qq.com>
2017-06-30 19:52:47 +0800

    退出登录功能共完成

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,13 +1,27 @@
 define(["jquery","template","cookie"],function ($,template) {
+	$(function () {
+		if("/dashboard/login" != location.pathname){
 
-	if("/dashboard/login" != location.pathname){
-		$(function () {
-			var userInfo = JSON.parse($.cookie("userinfo"));
-			console.log(userInfo)
-			var html = template("profile-tpl",userInfo);
+				var userInfo = JSON.parse($.cookie("userinfo"));
+				console.log(userInfo)
+				var html = template("profile-tpl",userInfo);
 
-			$("#userinfo").html(html);
+				$("#userinfo").html(html);
+
+			}
+
+
+		$("#logout").click(function () {
+			$.ajax({
+				url:"/api/logout",
+				type:"post",
+				success: function (data) {
+					if(data.code == 200){
+						location.href = "/dashboard/login"
+					}
+				}
+			})
 		})
-	}
+	})
 
 })
